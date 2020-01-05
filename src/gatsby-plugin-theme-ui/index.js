99814b0486b25f3b2813ddc8322e7b0376e799a7
tddpt <todd.peterson@gmx.com>
2020-01-05 16:36:23 -0500

    Colors Colors

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,142 @@
+import { tailwind } from "@theme-ui/presets"
+
+
+const headingStyles = {
+  h1: {
+    ...tailwind.styles.h1,
+    color: `heading`,
+    fontSize: [5, 6, 7],
+    fontWeight: `bold`,
+    mt: 2,
+  },
+  h2: {
+    ...tailwind.styles.h2,
+    color: `heading`,
+    fontSize: [4, 5, 6],
+    mt: 2,
+  },
+  h3: {
+    ...tailwind.styles.h3,
+    color: `heading`,
+    fontSize: [3, 4, 5],
+    mt: 3,
+  },
+  h4: {
+    ...tailwind.styles.h4,
+    color: `heading`,
+    fontSize: [2, 3, 4],
+  },
+  h5: {
+    ...tailwind.styles.h5,
+    color: `heading`,
+    fontSize: [1, 2, 3],
+  },
+  h6: {
+    ...tailwind.styles.h6,
+    color: `heading`,
+    fontSize: 1,
+    mb: 2,
+  },
+}
+
+export default {
+  ...tailwind,
+  initialColorMode: `light`,
+  useCustomProperties: true,
+  colors: {
+    ...tailwind.colors,
+    text: `#A2405E`,
+    primary: `#8C2F49`,
+    secondary: `#B85374`,
+    toggleIcon: `#8C2F49`,
+    heading: `#8C2F49`,
+    divide: `#8C2F49`,
+    background: `#C9C9C1`,
+    modes: {
+      dark: {
+        text: `#EEEEE9`,
+        primary: `#C9C9C1`,
+        secondary: `#FFFFFF`,
+        toggleIcon: `#EEEEE9`,
+        background: `#A2405E`,
+        heading: `#C9C9C1`,
+        divide: `#A4A49B`,
+      },
+    },
+  },
+  fonts: {
+    ...tailwind.fonts,
+    body: '"Open Sans", sans-serif',
+    heading: '"Ubuntu", sans-serif',
+    monospace: '"Ubuntu Mono", monospace',
+  },
+  styles: {
+    ...tailwind.styles,
+    root: {
+      ...tailwind.styles.root,
+      color: `text`,
+      backgroundColor: `background`,
+    },
+    p: {
+      fontSize: [1, 1, 2],
+      letterSpacing: `-0.003em`,
+      lineHeight: `body`,
+      "--baseline-multiplier": 0.179,
+      "--x-height-multiplier": 0.35,
+    },
+    ...headingStyles,
+    Container: {
+      padding: [3, 4],
+    },
+    blockquote: {
+      borderLeftColor: `primary`,
+      borderLeftStyle: `solid`,
+      borderLeftWidth: `6px`,
+      mx: 0,
+      pl: 4,
+      p: {
+        fontStyle: `italic`,
+      },
+    },
+  },
+  text: {
+    ...headingStyles,
+    heading: {
+      fontFamily: `heading`,
+      fontWeight: `heading`,
+      lineHeight: `heading`,
+      color: `heading`,
+    },
+  },
+  dividers: {
+    bottom: {
+      borderBottomStyle: `solid`,
+      borderBottomWidth: `1px`,
+      borderBottomColor: `divide`,
+      pb: 3,
+    },
+    top: {
+      borderTopStyle: `solid`,
+      borderTopWidth: `1px`,
+      borderTopColor: `divide`,
+      pt: 3,
+    },
+  },
+  links: {
+    secondary: {
+      color: `secondary`,
+      textDecoration: `none`,
+      ":hover": {
+        color: `heading`,
+        textDecoration: `underline`,
+      },
+      ":focus": {
+        color: `heading`,
+      },
+    },
+    listItem: {
+      fontSize: [1, 2, 3],
+      color: `text`,
+    },
+  },
+}
