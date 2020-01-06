253bb4abe033b943c4bf25ebde5ed62362b01c1f
tddpt <todd.peterson@gmx.com>
2020-01-06 16:41:11 -0500

    Fart poster

---
+++ .history/gatsby-config_20200106163915.js
@@ -0,0 +1,61 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Todd P.`,
+
+    siteTitleAlt: `100 Days of Gatsby`,
+    navigation: [
+      {
+        title: `Blog`,
+        slug: `/blog`,
+      },
+      {
+        title: `About`,
+        slug: `/about`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Todd P - 100 Days of Gatsby Blogs`,
+        short_name: `gatsby-blog`,
+        description: `Typography driven `,
+        start_url: `/`,
+        background_color: `#EEEEE9`,
+        theme_color: `#8C2F49`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    // `gatsby-plugin-webpack-bundle-analyser-v2`,
+  ],
+}
---
+++ .history/src/@lekoarts/gatsby-theme-minimal-blog/components/title_20200106112620.tsx
@@ -0,0 +1,44 @@
+/** @jsx jsx */
+import React from "react"
+import { jsx } from "theme-ui"
+import { Heading } from "@theme-ui/components"
+
+type TitleProps = {
+  children?: React.ReactNode
+  as?: string
+  className?: string
+  text: string
+}
+
+const Title = ({ text, children, as = `h2`, className }: TitleProps) => (
+  <div
+    sx={{
+      justifyContent: `space-between`,
+      alignItems: `center`,
+      borderBottomStyle: `solid`,
+      borderBottomWidth: `1px`,
+      borderBottomColor: `divide`,
+      pb: 3,
+      mb: ,
+      flexFlow: `wrap`,
+      boxSizing: `border-box`,
+      display: `flex`,
+    }}
+  >
+    <Heading as={as} sx={{ fontWeight: `medium`, fontSize: [4, 5], fontFamily: `heading` }} className={className}>
+      {text}
+    </Heading>
+    <div
+      sx={{
+        color: `secondary`,
+        a: {
+          variant: `links.secondary`,
+        },
+      }}
+    >
+      {children}
+    </div>
+  </div>
+)
+
+export default Title
